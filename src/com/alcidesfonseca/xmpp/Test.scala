53cce7cd3c1baf6b82f678aa5ebf7cfdf8ad2ccb
Alcides Fonseca <me@alcidesfonseca.com>
2008-10-05 15:58:05 +0100

    Added directory structure with Ant tasks

---
+++ src/com/alcidesfonseca/xmpp/Test.scala
@@ -0,0 +1,14 @@
+//
+//  Test
+//
+//  Created by  on 2008-10-05.
+//  Copyright (c) 2008 Alcides Fonseca. All rights reserved.
+//
+
+package com.alcidesfonseca.xmpp;
+
+object Test {
+	def main(args: Array[String]) {
+		println("Hello, world!")
+	}
+}
---
+++ src/sd/Boot.scala
@@ -0,0 +1,14 @@
+//
+//  Boot
+//
+//  Created by  on 2008-10-05.
+//  Copyright (c) 2008 Alcides Fonseca. All rights reserved.
+//
+
+import com.alcidesfonseca.xmpp._
+
+object Boot {
+	def main(args: Array[String]) {
+		Test.main(null)
+	}
+}
