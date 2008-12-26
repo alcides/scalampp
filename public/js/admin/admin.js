7797a0265b888de7753ae352a900e6198ff1c316
Alcides Fonseca <me@alcidesfonseca.com>
2008-12-26 12:01:42 +0000

    Loads server info without sorting

--- public/js/admin/admin.js
+++ public/js/admin/admin.js
@@ -1,3 +1,28 @@
+var prefix = "/admin/";
+var servers = [];
+
+var populate_servers = function() {
+	$("servers_table").update();
+	h = [];
+	servers.each(function(i){
+		i['memory'] = Math.round(i.memory*100)/100
+		h.push(server_row_template.evaluate(i));
+	});
+	$("servers_table").innerHTML = h.join("");
+};
+
+
+var get_servers = function() {
+	$get("servers",function(r) {
+		servers = r.servers;
+		populate_servers();
+	});
+	
+};
+
+
 document.observe('dom:loaded',function() {
-	new Control.Tabs('tabs_admin');  
+	new Control.Tabs('tabs_admin');
+	get_servers();
+	new PeriodicalExecuter(get_servers, 5);
 });
---
+++ public/js/admin/templates.js
@@ -0,0 +1,1 @@
+var server_row_template = new Template('<tr><td>#{address}</td><td>#{cpu}</td><td>#{memory}</td><td>#{network}</td><td>#{jids.length}</td></tr>');
